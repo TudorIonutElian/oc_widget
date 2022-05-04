c7d0a63643d0e8efb80bb2c0c32ccf25ace4c1d4
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-05-04 23:54:17 +0300

    Widget is working

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let status = 'not_loaded_news';
 
     function splitNewsArrayIntoPages(elements, elementsIntoPage) {
         const res = [];
@@ -10,6 +11,7 @@ $(document).ready(function() {
     }
 
     function showNewsOnWidget(newsParam) {
+        $('.news-list').empty();
         [...newsParam].forEach(newsElement => {
             let html = `<div class="news">
                             <div class="news_title">
@@ -35,6 +37,27 @@ $(document).ready(function() {
         }
     }
 
+    function getNewsFromAPI(url, method) {
+        // GET THE NEWS FROM THE API
+        $.ajax({
+            type: method,
+            url: url,
+            dataType: "json",
+            success: function(data) {
+                news.pages = splitNewsArrayIntoPages(data.news, 5);
+                showNewsOnWidget(news.pages[paginaCurenta - 1]);
+                renderDotsOnWidget(news.pages.length);
+                if (data.length > 0) {
+                    status = 'loaded_news'
+                }
+            },
+            error: function(errormessage) {
+                console.log(errormessage);
+            }
+        });
+
+    }
+
     //definirea paginii curente
     let paginaCurenta = 1;
 
@@ -42,23 +65,34 @@ $(document).ready(function() {
         pages: undefined
     };
 
-    let errors = [];
+    getNewsFromAPI("http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8", "GET");
 
-    // GET THE NEWS FROM THE API
-    $.ajax({
-        type: "GET",
-        url: "http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8",
-        dataType: "json",
-        success: function(data) {
-            news.pages = splitNewsArrayIntoPages(data.news, 5);
-            showNewsOnWidget(news.pages[paginaCurenta - 1]);
-            renderDotsOnWidget(news.pages.length)
-        },
-        error: function(errormessage) {
-            console.log(errormessage);
+    $(document).on('click', '.wtr_dot', function(e) {
+        // eliminare clase active de pe celelalte elemente
+        $('.wtr_dot.dot-active').removeClass('dot-active')
+
+        // setare clasa activa
+        $(e.currentTarget).addClass('dot-active')
+        paginaCurenta = parseInt($(e.currentTarget).attr('data-pagina-id'));
+        showNewsOnWidget(news.pages[paginaCurenta - 1]);
+    })
+
+    setTimeout(() => {
+        getNewsFromAPI();
+    }, 18000);
+
+    setInterval(function() {
+        if (paginaCurenta < news.pages.length) {
+            paginaCurenta = paginaCurenta + 1;
+
+        } else if (paginaCurenta == news.pages.length) {
+            paginaCurenta = 1;
         }
-    });
+
+        $($($('.wtr_dot.dot-active')[0])).removeClass('dot-active');
+        $($(`.wtr_dot[data-pagina-id=${paginaCurenta}]`)[0]).addClass('dot-active');
+        showNewsOnWidget(news.pages[paginaCurenta - 1])
 
 
-
+    }, 15000);
 });
